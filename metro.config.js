e174261d000cc07d35f61b81212ba4e978d8bfcb
dudgeon <dudgeon@gmail.com>
2025-06-15 17:11:37 -0400

    fix(nativewind): specify tailwind config path

--- metro.config.js
+++ metro.config.js
@@ -2,6 +2,9 @@ const { getDefaultConfig } = require("expo/metro-config");
 const { withNativeWind }  = require("nativewind/metro");
 
 const config = getDefaultConfig(__dirname);
+const path = require("path");
+
 module.exports = withNativeWind(config, {
-  input: "./global.css"
+  input: "./global.css",
+  configPath: path.join(__dirname, "tailwind.config.js")
 });
